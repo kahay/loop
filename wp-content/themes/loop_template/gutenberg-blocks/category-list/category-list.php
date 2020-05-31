610f10173ac8e409b29df86f543a214c7c33fb94
Kamilek <kamil.bartusik@sky-shop.pl>
2020-06-01 00:54:59 +0200

    Naprawienie wyswietlania sidebara na podstronie kategorii

--- wp-content/themes/loop_template/gutenberg-blocks/category-list/category-list.php
+++ wp-content/themes/loop_template/gutenberg-blocks/category-list/category-list.php
@@ -1,10 +1,10 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-            <?php get_template_part('template-parts\sidebar'); ?>
+            <?php get_template_part('template', 'parts/sidebar'); ?>
         </div>
         <div class="col-md-9">
-            <?php get_template_part('template-parts\products', 'list'); ?>
+            <?php get_template_part('template-parts/products', 'list'); ?>
         </div>
     </div>
 </div>
